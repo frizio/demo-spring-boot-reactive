c6e5a3c53ea4c8c012de1b6e43135cd05690b663
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-12-09 12:36:35 +0100

    Filtering a Reactive stream

---
+++ src/test/java/cloud/frizio/dev/demospringbootreactive/demospringbootreactive/fluxAndMonoPlayground/FluxFiltering.java
@@ -0,0 +1,37 @@
+package cloud.frizio.dev.demospringbootreactive.demospringbootreactive.fluxAndMonoPlayground;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+/**
+ * FluxFiltering
+ */
+
+public class FluxFiltering {
+
+  List<String> namesList = Arrays.asList("Al", "Bob", "Carl", "Dan", "Carol", "Carter");
+
+  @Test
+  public void fluxWithFilter() {
+    Flux<String> namesFilteredFlux = Flux.fromIterable(this.namesList)
+      .filter(s -> s.startsWith("C"));
+    StepVerifier.create(namesFilteredFlux.log())
+      .expectNext("Carl", "Carol", "Carter")
+      .verifyComplete();
+  }
+
+  @Test
+  public void fluxWithFilterLenght() {
+    Flux<String> namesFilteredFlux = Flux.fromIterable(this.namesList)
+      .filter(s -> s.length() <= 3);
+    StepVerifier.create(namesFilteredFlux.log())
+      .expectNext("Al", "Bob", "Dan")
+      .verifyComplete();
+  }
+
+}
